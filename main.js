d8c14525326c88cfb7aebfd5f5c8fe7dfef2c290
aditi419 <76972940+aditi419@users.noreply.github.com>
2021-02-22 16:52:37 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+list_of_students=[];
+
+function submit() {
+
+    var name_1=document.getElementById("student_one").value;
+var name_2=document.getElementById("student_two").value;
+var name_3=document.getElementById("student_three").value;
+var name_4=document.getElementById("student_four").value;
+list_of_students.push(name_1);
+list_of_students.push(name_2);
+list_of_students.push(name_3;
+list_of_students.push(name_4);
+console.log(display_name);
+document.getElementById("submit_button").style.display = "none"; document.getElementById("sort_button").style.display = "inline-block";
+
+}
+
+function sorting()
+ { name_of_the_student_array.sort(); console.log(name_of_the_student_array); document.getElementById("display_name").innerHTML = name_of_the_student_array; }
+
+ 
